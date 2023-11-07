d851438950e2a2a28a6fee4250fb406965fffd8d
Nazar Antoniuk <antoniuk.nazar09@gmail.com>
2023-11-07 07:55:32 +0200

    implements auto-disposing

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -1,2 +1,2 @@
 export * from "https://lib.deno.dev/x/grammy@1.x/mod.ts";
-export * from "https://esm.sh/awilix@8.x";
+export * from "https://esm.sh/awilix@9.x";
--- src/mod.ts
+++ src/mod.ts
@@ -1,20 +1,21 @@
 import { type AwilixContainer, Context, type MiddlewareFn } from "./deps.deno.ts";
 
-// deno-lint-ignore ban-types, no-explicit-any
+// deno-lint-ignore no-explicit-any
 export type AwilixFlavor<Cradle extends object = any> = {
   diContainerScope: AwilixContainer<Cradle>;
 };
+export type AwilixOptions = { disposeOnReply: boolean };
 
-// TODO: add configuration
 export function awilix<C extends Context = Context>(
   diContainer: AwilixFlavor["diContainerScope"],
+  options?: AwilixOptions,
 ): MiddlewareFn<C & AwilixFlavor> {
   return async (ctx, next) => {
     ctx.diContainerScope = diContainer.createScope();
-    // ctx.api.config.use(async (prev, method, payload, signal) => {
-    //   await ctx.diContainerScope.dispose();
-    //   return await prev(method, payload, signal);
-    // });
-    await next();
+    ctx.api.config.use(async (prev, method, payload, signal) => {
+      if (options?.disposeOnReply) await ctx.diContainerScope.dispose();
+      return await prev(method, payload, signal);
+    });
+    return await next();
   };
 }
